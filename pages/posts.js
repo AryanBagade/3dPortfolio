c62500d90de6bc696a9ec10dfdc844b9996b6492
Aaryan Bagade <aryanbagade@Aryans-MacBook-Pro.local>
2022-06-14 23:53:53 +0530

    commit 5

--- pages/posts.js
+++ pages/posts.js
@@ -3,7 +3,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbinProgress from '../public/images/works/wip.png'
+import thumbArticle1 from '../public/images/works/buildDapp.png'
 
 const Posts = () => (
   <Layout title="Posts">
@@ -15,7 +15,7 @@ const Posts = () => (
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <GridItem
             title="Build your first dApp from scratch (part-1)"
-            thumbnail="https://miro.medium.com/max/1400/1*f797kY4_Rn1lj0fZ8utFdQ.png"
+            thumbnail={thumbArticle1}
             href="https://medium.com/@AryanBagade/build-your-first-dapp-from-scratch-part-1-d04eddb02116"
           />
         </SimpleGrid>
